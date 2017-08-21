c5aca57a2edbbd724ebdc8df8a8aac80f441cff6
Desmonddai583 <desmonddai583@gmail.com>
2017-08-22 04:48:16 +0800

    effect and commands

--- Bingo.elm
+++ Bingo.elm
@@ -3,6 +3,7 @@ module Bingo exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Random
 
 -- MODEL
 
@@ -28,22 +29,23 @@ initialModel =
 
 initialEntries : List Entry
 initialEntries =
-    [ Entry 1 "Future-Proof" 100 False
-    , Entry 2 "Doing Agile" 200 False
-    , Entry 3 "In The Cloud" 300 False
+    [ Entry 3 "In The Cloud" 300 False
+    , Entry 1 "Future-Proof" 100 False
     , Entry 4 "Rock-Star Ninja" 400 False
+    , Entry 2 "Doing Agile" 200 False
     ]
 
 -- UPDATE
 
-type Msg = NewGame | Mark Int
+type Msg = NewGame | Mark Int | Sort | NewRandom Int
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NewRandom randomNumber ->
+            ( { model | gameNumber = randomNumber }, Cmd.none )
         NewGame ->
-            { model | gameNumber = model.gameNumber + 1,
-                entries = initialEntries }
+            ( { model | entries = initialEntries }, generateRandomNumber )
         Mark id ->
             let
                 markEntry e =
@@ -52,7 +54,15 @@ update msg model =
                     else
                         e
             in
-                { model | entries = List.map markEntry model.entries }
+                ( { model | entries = List.map markEntry model.entries }, Cmd.none )
+        Sort ->
+            ( { model | entries = List.sortBy .points model.entries }, Cmd.none )
+
+-- COMMANDS
+
+generateRandomNumber : Cmd Msg
+generateRandomNumber =
+    Random.generate NewRandom (Random.int 1 100)
 
 -- VIEW
 
@@ -96,23 +106,44 @@ viewEntryList entries =
         |> List.map viewEntryItem
         |> ul []
 
+allEntriesMarked : List Entry -> Bool
+allEntriesMarked entries =
+    List.all .marked entries
+
+sumMarkedPoints : List Entry -> Int
+sumMarkedPoints entries =
+    entries
+        |> List.filter .marked
+        |> List.foldl (\e sum -> sum + e.points) 0
+
+viewScore : Int -> Html Msg
+viewScore sum =
+    div
+        [ class "score" ]
+        [ span [ class "label" ] [ text "Score" ]
+        , span [ class "value" ] [ text (toString sum) ]
+        ]
+
 view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ viewHeader "BUZZWORD BINGO"
         , viewPlayer model.name model.gameNumber
         , viewEntryList model.entries
+        , viewScore (sumMarkedPoints model.entries)
         , div [ class "button-group" ]
-                [ button [ onClick NewGame ] [ text "New Game" ] ]
+                [ button [ onClick NewGame ] [ text "New Game" ]
+                , button [ onClick Sort ] [ text "Sort" ]
+                ]
         , div [ class "debug" ] [ text (toString model) ]
         , viewFooter
         ]
 
 main : Program Never Model Msg
 main =
-    beginnerProgram
-        { model = initialModel
+    program
+        { init = ( initialModel, generateRandomNumber )
         , view = view
         , update = update
+        , subscriptions = (\_ -> Sub.none)
         }
-
