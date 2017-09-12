eae72559552ad69504a4dd02d4cf02bd4ba022d2
desmonddai583 <desmonddai583@gmail.com>
2017-09-12 18:20:13 +0800

    full handling for error

--- Bingo.elm
+++ Bingo.elm
@@ -29,7 +29,7 @@ initialModel =
     { name = "Desmond"
     , gameNumber = 1
     , entries = []
-    , alertMessage = Just "Oops!"
+    , alertMessage = Nothing
     }
 
 -- UPDATE
@@ -53,9 +53,30 @@ update msg model =
             ( { model | entries = List.sortBy .points randomEntries }, Cmd.none )
         NewEntries (Err error) ->
             let
-                _ = Debug.log "Oops" error
+                errorMessage =
+                    case error of
+                        Http.NetworkError ->
+                            "Is the server running?"
+
+                        Http.Timeout ->
+                            "Request timed out!"
+
+                        Http.BadUrl url ->
+                            ("Invalid URL: " ++ url)
+
+                        Http.BadStatus response ->
+                            case response.status.code of
+                                401 ->
+                                    "Unauthorized"
+                                404 ->
+                                    "Not Found"
+                                code ->
+                                    (toString code)
+
+                        Http.BadPayload reason response ->
+                            reason
             in
-                (model, Cmd.none)
+                ( { model | alertMessage = Just errorMessage }, Cmd.none )
         CloseAlert ->
             ( { model | alertMessage = Nothing }, Cmd.none )
         Mark id ->
